ae2e38785ad628e72a064272c5ebbf40c6c00ed9
Alexiuce <alexiuce@qq.com>
2018-02-21 19:16:18 +0800

    add test

---
+++ templates/swift/Example/PROJECT_Tests/PROJECT_Tests.swift
@@ -0,0 +1,35 @@
+//
+//  PROJECT_ExampleTests.swift
+//  PROJECT_ExampleTests
+//
+//  Created by Alexcai on 2018/2/21.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+import XCTest
+
+class PROJECT_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
