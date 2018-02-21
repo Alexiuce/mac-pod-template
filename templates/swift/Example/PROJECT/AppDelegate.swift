8f6d73856e45fcec9f70f997e39b6350a6fa63eb
Alexiuce <alexiuce@qq.com>
2018-02-21 17:38:19 +0800

    add mac -pod -template

---
+++ templates/swift/Example/PROJECT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PROJECT
+//
+//  Created by Alexcai on 2018/2/21.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ templates/swift/Example/PROJECT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PROJECT
+//
+//  Created by Alexcai on 2018/2/21.
+//  Copyright © 2018年 Alexcai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
